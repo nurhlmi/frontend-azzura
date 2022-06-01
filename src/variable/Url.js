b9e969d26cd3864aadf5c08f82b6ce0688c9b7cf
root <root@vps.server.local>
2022-06-01 14:13:50 +0000

    delete Url

--- src/variable/Url.js
+++
@@ -1,6 +0,0 @@
-const baseUrl = `http://localhost:8000`;
-const apiUrl = `${baseUrl}/api`;
-const title = ` | Azzura`;
-const token = localStorage.getItem("token");
-
-export { baseUrl, apiUrl, title, token };
